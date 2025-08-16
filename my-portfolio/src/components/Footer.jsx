9307f4627026db001fb7bf6b86c92a53d555e49b
[vinald] <okiror1vinald@gmail.com>
2025-08-17 02:20:07 +0300

    Updates

--- my-portfolio/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-        <footer className="bg-dark text-light text-center py-2">
-            <p>&copy; {new Date().getFullYear()} Okiror Samuel Vinald</p>
-        </footer>
-    );
-}
-export default Footer;
