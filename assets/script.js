dc63496fa5a4f88e7ad3b80626d021e2daa22a22
[vinald] <okiror1vinald@gmail.com>
2025-08-17 00:34:31 +0300

    improve the menu bar displaying on mobile

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,16 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("currentYear").textContent = new Date().getFullYear();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navbarCollapse = document.getElementById("navbarNav");
+    var navLinks = navbarCollapse.querySelectorAll(".nav-link");
+    var bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            if (window.innerWidth < 768 && navbarCollapse.classList.contains("show")) {
+                bsCollapse.hide();
+            }
+        });
+    });
+});
