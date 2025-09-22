c6f9ad636381e7a7a079936ba206a70ee5309f64
[vinald] <okiror1vinald@gmail.com>
2025-09-22 12:34:53 +0300

    portifolio todo and recipe projects

---
+++ Todo-App/TodoApp/src/App.jsx
@@ -0,0 +1,12 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoList from "./components/todoList/TodoList.jsx";
+function App() {
+    return (
+        <>
+            <TodoList />
+        </>
+    );
+}
+
+export default App;
